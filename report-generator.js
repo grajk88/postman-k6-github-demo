b762b966825fd878d42425dfd7c7b49a7e76fad6
grajk88 <vgrk.2017@gmail.com>
2023-11-26 22:14:27 +0000

    in progress - added html report

---
+++ report-generator.js
@@ -0,0 +1,8 @@
+const reporter = require('k6-html-reporter');
+
+const options = {
+        jsonFile: "results.json",
+        output: "report.html",
+    };
+
+reporter.generateSummaryReport(options);
